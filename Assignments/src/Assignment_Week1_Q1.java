1b7e9235a6e256389ad5d440a1e69aec143a1d9d
Shashank <shashank.sj2008@gmail.com>
2023-02-24 15:57:14 +0530

    Code for question 1 for Assignment week1

---
+++ Assignments/src/Assignment_Week1_Q1.java
@@ -0,0 +1,7 @@
+
+public class Assignment_Week1_Q1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Core Java!");
+		System.out.println("=====================");
+	}
+}
